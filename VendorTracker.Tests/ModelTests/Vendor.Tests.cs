7fd7c1e8e26c514eb36b0102c5bdb29652215986
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-01-26 12:16:29 -0600

    Test 16 get Vendor description passed

--- VendorTracker.Tests/ModelTests/Vendor.Tests.cs
+++ VendorTracker.Tests/ModelTests/Vendor.Tests.cs
@@ -11,16 +11,24 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("Test Vendor");
+      Vendor newVendor = new Vendor("Test Vendor", "Test Description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
     [TestMethod]
     public void GetName_ReturnsName_String()
     {
       string name = "Test Vendor";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "Test Description");
       string result = newVendor.Name;
       Assert.AreEqual(name, result);
+    }
+    [TestMethod]
+    public void GetVendorDescription_ReturnsVendorDescriptionValue_String()
+    {
+      string description = "Orders every tuesday";
+      Vendor newVendor = new Vendor("bread", description);
+      string result = newVendor.Description;
+      Assert.AreEqual(description, result);
     }
   }
 }
--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -4,10 +4,12 @@ namespace VendorTracker.Models
   public class Vendor
   {
     public string Name {get;}
+    public string Description {get;}
   
-    public Vendor(string name)
+    public Vendor(string name, string description)
     {
       Name = name;
+      Description = description;
     }
   }
 }
