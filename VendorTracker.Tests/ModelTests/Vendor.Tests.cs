997b2657ea8e37d0702f8e534686ee7d207a6f95
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-01-26 12:09:18 -0600

    Test 14 passed Vendor Constructor

---
+++ VendorTracker.Tests/ModelTests/Vendor.Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorTracker.Models;
+using System.Collections.Generic;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor();
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
